089f36dcfbeac21ff3a5db0b7fe7d1788f5d97bb
KlnSdr <59069848+KlnSdr@users.noreply.github.com>
2021-12-05 19:43:21 +0100

    added favicon

--- ui.ts
+++ ui.ts
@@ -1,5 +1,18 @@
 function createUI() {
-    document.title = 'mission control';
+    setTitle();
+    setFavicon();
     Content.render();
     Navbar.render();
+}
+
+function setTitle() {
+    document.title = 'mission control';
+}
+
+function setFavicon() {
+    // set favicon dynamically for all pages
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = './favicon.svg';
+    document.getElementsByTagName('head')[0].appendChild(link);
 }
