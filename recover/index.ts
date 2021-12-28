aa9b3923d3651fcc3fa0bc27fda4b1d6c0e24059
KlnSdr <59069848+KlnSdr@users.noreply.github.com>
2021-12-28 23:10:41 +0100

    reloading a snapshot from a link is now possible

--- recover/index.ts
+++ recover/index.ts
@@ -1,3 +1,13 @@
 function recover() {
-    
+    const rawUrl: string = document.URL;
+    const url: URL = new URL(rawUrl);
+    const data: string | null = url.searchParams.get('data');
+
+    if (data != null) {
+        const parsedData: obj = JSON.parse(atob(data));
+        Datahandler.saveData('missions', parsedData.missions);
+        Datahandler.saveData('vehicles', parsedData.vehicles);
+    }
+
+    location.assign('../');
 }
